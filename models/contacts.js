75cd4c23788648435c513ddd69a725794d7dfbf8
Roman Radakhevych <akrir@ukr.net>
2022-10-26 22:46:36 +0300

    fn 1

--- models/contacts.js
+++ models/contacts.js
@@ -1,23 +1,44 @@
-// const fs = require('fs/promises')
+const fs = require("fs/promises");
+const path = require("path");
+const contactsPath = path.join(__dirname, "contacts.json");
 
 const listContacts = async () => {
-  return "FFFFF";
+  const data = await fs.readFile(contactsPath);
+  const contacts = JSON.parse(data);
+  return contacts;
 };
 
 const getContactById = async (contactId) => {
-  return contactId;
+  const data = await listContacts();
+  const searchedContact = data.find((c) => c.id === contactId);
+  if (!searchedContact) return null;
+  return searchedContact;
 };
 
 const removeContact = async (contactId) => {
-  return contactId;
+  const data = await listContacts();
+  const index = data.findIndex((c) => c.id === contactId);
+  if (index === -1) return null;
+  const [contactToRemove] = data.splice(index, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(data));
+  return contactToRemove;
 };
 
 const addContact = async (body) => {
+  const data = await listContacts();
+  data.push(body);
+  await fs.writeFile(contactsPath, JSON.stringify(data));
   return body;
 };
 
 const updateContact = async (contactId, body) => {
-  return { contactId, ...body };
+  const data = await getContactById(contactId);
+  if (!data) return null;
+  if (body.name) data.name = body.name;
+  if (body.phone) data.phone = body.phone;
+  if (body.email) data.email = body.email;
+
+  return data;
 };
 
 module.exports = {
