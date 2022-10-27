77743043ff5e4a9639b6abc6cd9430c2597ed904
Roman Radakhevych <akrir@ukr.net>
2022-10-27 18:40:45 +0300

    fn 2

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -4,18 +4,17 @@ const { v4: uuidv4 } = require("uuid");
 const contacts = require("./../../models/contacts");
 
 router.get("/", async (req, res, next) => {
-  const data = await contacts.listContacts();
-  res.status(200);
-  res.json({ data });
+  const result = await contacts.listContacts();
+  res.status(200).json({ result });
 });
 
 router.get("/:contactId", async (req, res, next) => {
-  const id = req.params.contactId;
-  const data = await contacts.getContactById(id);
-  if (!data) {
+  const { contactId } = req.params;
+  const result = await contacts.getContactById(contactId);
+  if (!result) {
     return res.status(404).json({ message: "Not found" });
   }
-  res.status(200).json({ data });
+  res.status(200).json({ result });
 });
 
 router.post("/", async (req, res, next) => {
@@ -23,15 +22,15 @@ router.post("/", async (req, res, next) => {
   if (!name || !email || !phone) {
     return res.status(400).json({ message: "missing required name field" });
   }
-  const data = { id: uuidv4(), name, email, phone };
-  await contacts.addContact(data);
-  res.status(201).json({ data });
+  const newContact = { id: uuidv4(), name, email, phone };
+  await contacts.addContact(newContact);
+  res.status(201).json({ result: newContact });
 });
 
 router.delete("/:contactId", async (req, res, next) => {
-  const id = req.params.contactId;
-  const data = await contacts.removeContact(id);
-  if (!data) {
+  const { contactId } = req.params;
+  const contactToRemove = await contacts.removeContact(contactId);
+  if (!contactToRemove) {
     return res.status(404).json({ message: "Not found" });
   }
   res.status(200).json({ message: "contact deleted" });
@@ -43,11 +42,15 @@ router.put("/:contactId", async (req, res, next) => {
   if (!name && !email && !phone) {
     return res.status(400).json({ message: "missing fields" });
   }
-  const data = await contacts.updateContact(id, { name, email, phone });
-  if (!data) {
+  const contactToUpdate = await contacts.updateContact(id, {
+    name,
+    email,
+    phone,
+  });
+  if (!contactToUpdate) {
     return res.status(404).json({ message: "Not found" });
   }
-  res.status(200).json({ data });
+  res.status(200).json({ reult: contactToUpdate });
 });
 
 module.exports = router;
