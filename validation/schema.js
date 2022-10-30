d7087d099c6a1440540ae6d8d2b83457649c1a98
Roman Radakhevych <akrir@ukr.net>
2022-10-30 11:58:37 +0200

    fix scheme

--- validation/schema.js
+++ validation/schema.js
@@ -1,6 +1,19 @@
 const Joi = require("joi");
 
-const schema = Joi.object({
+const schemaPostContact = Joi.object({
+  name: Joi.string()
+    .min(3)
+    .pattern(/^[A-Za-z\s.\-']*$/)
+    .required(),
+  email: Joi.string()
+    .email({ minDomainSegments: 2, tlds: { deny: ["ru"] } })
+    .required(),
+  phone: Joi.string()
+    .pattern(/^(?:\d{3}|\(\d{3}\))\s\d{3}([-\s])\d{2}\1\d{2}$/)
+    .required(),
+});
+
+const schemaPutContact = Joi.object({
   name: Joi.string()
     .min(3)
     .pattern(/^[A-Za-z\s.\-']*$/),
@@ -8,15 +21,7 @@ const schema = Joi.object({
   phone: Joi.string().pattern(
     /^(?:\d{3}|\(\d{3}\))\s\d{3}([-\s])\d{2}\1\d{2}$/
   ),
-});
-
-const schemaPostContact = schema.append({
-  name: Joi.required(),
-  email: Joi.required(),
-  phone: Joi.required(),
-});
-
-const schemaPutContact = schema.or("name", "email", "phone");
+}).or("name", "email", "phone");
 
 module.exports = {
   schemaPostContact,
